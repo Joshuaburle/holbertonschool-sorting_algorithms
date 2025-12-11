ad0924d5ef28f369393084b2078da770b5154896
Joshuaburle <11491@holbertonschool.com>
2025-12-11 09:55:02 +0100

    Modify for loop on file 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,7 +3,7 @@
 /**
  * bubble_sort - Sorts an array of integers in ascending order
  *               using the bubble sort algorithm
- * @array: Pointer to the array to sort
+ * @array: Array of integers to sort
  * @size: Size of the array
  *
  * Return: Nothing
@@ -11,16 +11,14 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp, swapped;
+	int tmp;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		swapped = 0;
-
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -28,12 +26,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 				print_array(array, size);
-				swapped = 1;
 			}
 		}
-
-		if (!swapped)
-			return;
 	}
 }
-
