a3bec3fedd8e183680a6486f6509e698b242f076
Joshuaburle <11491@holbertonschool.com>
2025-12-12 10:38:23 +0100

    Modify code on file 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,7 +2,7 @@
 
 /**
  * selection_sort - Sorts an array of integers in ascending order
- *                  using the selection sort algorithm
+ *                  using the Selection sort algorithm
  * @array: Array to sort
  * @size: Size of the array
  *
@@ -10,7 +10,7 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, min_idx;
 	int tmp;
 
 	if (array == NULL || size < 2)
@@ -18,15 +18,20 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
+		min_idx = i;
+
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[i])
-			{
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
-				print_array(array, size);
-			}
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+
+		if (min_idx != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
 		}
 	}
 }
